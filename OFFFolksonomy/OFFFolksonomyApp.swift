f67263a9a1b9d9c09327fd05df72357cdccbd8b2
Arnaud Leene <aleene@gmail.com>
2022-10-13 16:50:08 +0200

    Initial Commit

---
+++ OFFFolksonomy/OFFFolksonomyApp.swift
@@ -0,0 +1,17 @@
+//
+//  OFFFolksonomyApp.swift
+//  OFFFolksonomy
+//
+//  Created by Arnaud Leene on 13/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct OFFFolksonomyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
